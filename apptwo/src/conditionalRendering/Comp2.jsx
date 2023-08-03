e8da5a870c2edaf4b5cff74f373c8555a5652442
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-03 19:58:27 +0530

    conditional rendering

---
+++ apptwo/src/conditionalRendering/Comp2.jsx
@@ -0,0 +1,8 @@
+// conditional rendring  refers to render a component based on condition (true/false).
+
+let Comp2=()=>{
+    return<>
+            <h1>Conditional rendering comp2</h1>
+         </>
+}
+export default Comp2;
--- apptwo/src/store.jsx
+++
@@ -1,7 +0,0 @@
-import {createStore} from "redux"
-import {composeWithDevTools} from "@redux-devtools/extension"
-import {Reducerfun} from "./Reducerfun"
-
-let Store=createStore(Reducerfun,composeWithDevTools())
-
-export {Store};
