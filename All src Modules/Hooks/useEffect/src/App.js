fd52dd7d518121768efae200795d8e20a380e312
Sivareddy0930 <sivareddy0930@gmail.com>
2023-08-05 19:39:25 +0530

    useEffect

---
+++ All src Modules/Hooks/useEffect/src/App.js
@@ -0,0 +1,16 @@
+import Fun from "./useEffect/Fun"
+import Fun1 from "./useEffect/Fun1"
+import Fun2 from "./useEffect/Fun2"
+import Fun3 from "./useEffect/Fun3"
+function App() {
+  return  <>
+         {/* <Fun/> */}
+         {/* <Fun1/> */}
+         {/* <Fun2/> */}
+         <Fun3/>
+
+            
+            </>
+}
+
+export default App;
